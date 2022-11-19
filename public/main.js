42bcefb7eebee85104230ed938497ea6ee341909
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-11-19 13:53:27 +0000

    initial

--- public/main.js
+++ public/main.js
@@ -1,4 +1,9 @@
-
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
 
 
 // === Mobile navigation menu dropdown
