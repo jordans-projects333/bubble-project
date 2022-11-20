06adb55f8c09db0b4640118ba9539982226c8060
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-11-20 06:54:03 +0000

    initial

--- public/products-grid-items.js
+++ public/products-grid-items.js
@@ -23,44 +23,61 @@ function sliderPosition(position){
 }
 
 sliderPosition(0)
-items = []
+let items = []
 // Creating the product grid from data
-function createGridItems(number){
+async function DataCollection(number){
     for(let i = 0; i < number; i++){
-        const template = document.querySelector("#grid-item-template")
-        const the_content = template.content.cloneNode(true)
-        fetch(`item${i}`)
+        // const template = document.querySelector("#grid-item-template")
+        // const the_content = template.content.cloneNode(true)
+        await fetch(`item${i}`)
             .then(response => response.json())
             .then(data => {
-                // div.innerHTML = data.name
-                // the_content.querySelector("a").href = `/product${i}`
-                the_content.querySelector("img").src = data.image
-                // content.querySelector(".addToCart").addEventListener("click", () => addToTheCart(data.image,data.name,data.price))
-                the_content.querySelector("h5").innerHTML = data.price + "<br>"+data.name
-                document.querySelector(".grid-container").appendChild(the_content)
-                let numberOfChildNodes = document.querySelector(".grid-container").childElementCount
-                let getContent = document.querySelector(".grid-container").children[numberOfChildNodes-1]
-                // console.log(getContent)
-                items.push({element: getContent, category: data.category})
+                // the_content.querySelector("img").src = data.image
+                // the_content.querySelector("h5").innerHTML = data.price + "<br>"+data.name
+                // document.querySelector(".grid-container").appendChild(the_content)
+                // let numberOfChildNodes = document.querySelector(".grid-container").childElementCount
+                // let getContent = document.querySelector(".grid-container").children[numberOfChildNodes-1]
+                items.push(
+                    {
+                        id: i,
+                        category: data.category,
+                        description: data.description,
+                        image: data.image,
+                        name: data.name,
+                        price: data.price
+                })
             })
             .catch(err => console.log(err))
     }
 }
+
+async function createGridItems(number){
+    for(let i = 0; i < number; i++){
+        const template = document.querySelector("#grid-item-template")
+        const the_content = template.content.cloneNode(true)
+        // console.log(items[i].name)
+
+        the_content.querySelector("img").src = items[i].image
+        the_content.querySelector("h5").innerHTML = items[i].price + "<br>"+items[i].name
+        document.querySelector(".grid-container").appendChild(the_content)
+    }
+}
 fetch("/length")
     .then(response => response.json())
-    .then(data => {
-        createGridItems(data)
+    .then(async data => {
+        await DataCollection(data)
         // for(let i = 0; i < data; i++){
         //     const template = document.querySelector("#grid-item-template")
         //     const the_content = template.content.cloneNode(true)
         //     document.querySelector(".grid-container").appendChild(the_content)
         // }
-        console.log("working on it")
+        return data
     })
-    .then(data => {
-        console.log("under construction")
-        // createGridItems(data)
+    .then(async data => {
+        console.log("data collected")
+        await createGridItems(data)
         document.querySelector(".loader").style.display = "none"
+        document.querySelector(".product-number").innerText = `(${data})`
     })
     .catch(err => console.log(err))
 
